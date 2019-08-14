ccafc3a9c759ec5f95b6407b2e57c77db790849a
r-kimura001 <torakichisite@gmail.com>
2019-08-14 09:59:37 +0900

    Vuexのストアを使用する準備完了

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,13 +3,16 @@ import Vue from 'vue'
 // ルーティングの定義をインポートする
 import router from './router'
 
+import store from './store'
 // ルートコンポーネントをインポートする
 import App from './App.vue'
+
 
 
 new Vue({
   el: '#app',
   router, //ルーティングの定義を読み込む
+  store,
   components: { App }, //ルートコンポーネントの使用を宣言
   template: '<App />' // ルートコンポーネントを描画する
 })
